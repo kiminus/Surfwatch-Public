7d79df973cb26737f5ff355dd26d538647379ff9
zilin <quitebig1@gmail.com>
2026-01-19 00:41:14 -0800

    initial esp32

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,18 +1,34 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Most AI-Thinker ESP32-CAM boards have a small LED on GPIO 4
+// (sometimes called "onboard LED" or flash LED indicator)
+#define LED_PIN 4
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  delay(200);                   // Give serial some time to settle
+
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);   // LED off at start (most modules: LOW = on)
+
+  Serial.println("\n=====================================");
+  Serial.println("  ESP32-CAM Minimal Test - Build OK  ");
+  Serial.println("=====================================");
+  Serial.println("If you see this → upload & serial works!");
+  Serial.println("Current time: " __DATE__ " " __TIME__);
+  Serial.println("=====================================\n");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // Simple blink
+  digitalWrite(LED_PIN, HIGH);
+  delay(400);
+  digitalWrite(LED_PIN, LOW);
+  delay(400);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  static int counter = 0;
+  counter++;
+  if (counter % 10 == 0) {
+    Serial.printf("Running... count = %d\n", counter);
+  }
 }
